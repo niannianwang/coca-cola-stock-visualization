d0f7dc8a5141951ead4d393d129bde680c4d41ab
samuellee77 <hsl023@ucsd.edu>
2024-05-12 13:16:46 -0700

    clean data & create simple clickbox

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,8 +1,7 @@
 <script>
-    import ToDo from '../components/ToDo.svelte';
-    import Graph from '../components/Graph.svelte';
     import { onMount } from 'svelte';
     import Papa from 'papaparse';
+    import * as d3 from 'd3';
 
     let stockData = [];
     let loading = true;
@@ -11,116 +10,64 @@
         const response = await fetch('Coca-Cola_stock_history.csv');
         const csvData = await response.text();
         stockData = Papa.parse(csvData, { header: true }).data;
+        stockData = stockData.map(d => {
+            return {
+                Date: d3.timeParse("%Y-%m-%d")(d.Date),
+                Open: +d.Open,
+                High: +d.High,
+                Low: +d.Low,
+                Close: +d.Close,
+                Volume: +d.Volume,
+            };
+        });
         loading = false;
+        console.log(stockData);
     });
+
+    function createLineGraph(desiredType) {
+        const svg = d3.select('svg');
+        const width = 500;
+        const height = 300;
+
+        // Define the scales
+        const xScale = d3
+            .scaleTime()
+            .domain(d3.extent(stockData, d => d.Date))
+            .range([0, width]);
+
+        const yScale = d3
+            .scaleLinear()
+            .domain([0, d3.max(stockData, d => +d[desiredType])])
+            .range([height, 0]);
+        
+        // Append the line to the SVG
+        // error here
+        svg
+            .append('path')
+            .datum(stockData)
+            .attr('fill', 'none')
+            .attr('stroke', 'steelblue')
+            .attr('stroke-width', 2)
+            .attr('d', d3.line()
+                .x(d => xScale(+d.Date))
+                .y(d => yScale(+d[desiredType]))
+            );
+    }
+    function showOpenGraph() {
+        return createLineGraph('Open');
+    }
 </script>
+
 {#if loading}
     <h2>Loading...</h2>
 {:else}
 <main>
-    <section class="graph">
-        <h2 style="margin-top: 15px">Visualization</h2>
-        
-        <Graph bind:parsedData={stockData}/>
-    </section>
+    <input type="checkbox" on:click={showOpenGraph}>Show Open Graph
+    <!-- <button on:click={() => createLineGraph('desiredType')}>Change Line Graph</button> -->
+    <svg></svg>
 </main>
 {/if}
 
-
 <style>
-    @import url("https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;700&display=swap");
-
-    :root {
-        --color-bg: #ffffff;
-        --color-outline: #c2c2c2;
-        --color-shadow: hsl(0, 0%, 0%, 0.1);
-        --color-text: #3f4252;
-        --color-bg-1: hsla(0, 0%, 0%, 0.2);
-        --color-shadow-1: hsl(0, 0%, 96%);
-    }
-
-    *,
-    *::before,
-    *::after {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-
-    main {
-        text-align: center;
-        font-family: "Nunito", sans-serif;
-        font-weight: 300;
-        line-height: 2;
-        font-size: 24px;
-        color: var(--color-text);
-        margin-top: 100px;
-    }
-
-    label,
-    input,
-    button {
-        font-family: inherit;
-        font-weight: inherit;
-        line-height: inherit;
-        font-size: 24px;
-        width: 100%;
-    }
-
-    input {
-        padding-left: 40px;
-        line-height: 72px;
-        font-style: italic;
-        border: none;
-    }
-
-    ::placeholder {
-        color: #9e9e9e;
-    }
-
-    h1 {
-        font-size: 72px;
-        font-weight: 300;
-        line-height: 2;
-    }
-
-    h2 {
-        font-size: 30px;
-        font-weight: 300;
-        line-height: 1.5;
-    }
-
-    .todos {
-        display: inline-block;
-        vertical-align:top;
-        width: 500px;
-        background-color: var(--color-bg);
-        border-radius: 5px;
-        border: 1px solid var(--color-outline);
-        box-shadow: 0 0 4px var(--color-shadow);
-    }
-
-    .graph {
-        display: inline-block;
-        margin-left: 50px;
-    }
-
-    .actions {
-        position: relative;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-    }
-    .actions:before {
-        content: "";
-        height: 40px;
-        position: absolute;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        box-shadow: 0 1px 1px var(--color-shadow-1), 0 8px 0 -3px var(--color-shadow-1),
-            0 9px 1px -3px var(--color-bg-1), 0 16px 0 -6px var(--color-shadow-1),
-            0 17px 2px -6px var(--color-bg-1);
-        z-index: -1;
-    }
+    /* Styles omitted for brevity */
 </style>
